9518389806b4bea24e648c4b8c92aaa888540c45
Biola <saratsmith968@gmail.com>
2024-05-23 18:13:06 +0100

    error-handling middleware added to products

--- products/index.js
+++ products/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const dotenv = require('dotenv')
+const { errorHandler } = require('./src/middlewares/handleError');
+const { notFound } = require('./src/middlewares/notFound');
 const connectDB = require('./src/database/connection');
 const productRoute = require('./src/api/productRoute');
 
-const Product = require('./src/database/model')
 
 const app = express();
 
@@ -13,44 +14,14 @@ dotenv.config();
 connectDB();
 
 
+
 app.use(express.json());
 app.use('/api', productRoute);
 
-/*
-app.post('/api', async (req,res) => {
 
-    const { name, price, image, brand, category, countInStock, numReviews, description } = req.body;
+app.use(notFound);
+app.use(errorHandler);
 
-    const product = await Product.create({
-      name,
-      price,
-      image,
-      brand,
-      category,
-      countInStock, 
-      numReviews, 
-      description 
-
-    })
-  
-  if (product) {
-    res.status(201).json({
-      _id: product._id,
-      name: product.name,
-      brand: product.brand,
-    })
-  } else {
-    res.status(400).json({"msg": "Invalid product data"})
-    //throw new Error('Invalid product data')
-  }
-})
-
-
-app.use('/', (req, res, next) => {
-    return res.status(200).json({"msg": "Hello from Products"})
-})
-
-*/
 
 
 app.listen(8001, () => {
